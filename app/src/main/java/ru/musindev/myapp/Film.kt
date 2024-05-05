cd5b12dbc848067b866c90bfe323e6606a6d3d2f
Yuris <yuris.musin@mail.ru>
2024-05-05 11:52:41 +0500

    first commit

---
+++ app/src/main/java/ru/musindev/myapp/Film.kt
@@ -0,0 +1,11 @@
+package ru.musindev.myapp
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Film(
+    val title: String,
+    val poster: Int,
+    val description: String
+) : Parcelable
