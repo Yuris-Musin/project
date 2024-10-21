1111528dc52b157cb0f85072b6ace18ba307aff6
Yuris <yuris.musin@mail.ru>
2024-10-21 21:25:24 +0500

    add notifications

--- app/src/main/java/ru/musindev/myapp/App.kt
+++ app/src/main/java/ru/musindev/myapp/App.kt
@@ -1,10 +1,14 @@
 package ru.musindev.myapp
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
 import ru.musindev.myapp.di.AppComponent
 import ru.musindev.myapp.di.DaggerAppComponent
 import ru.musindev.myapp.di.modules.DatabaseModule
 import ru.musindev.myapp.di.modules.DomainModule
+import ru.musindev.myapp.view.notifications.NotificationConstants.CHANNEL_ID
 import ru.musindev.remote_module.DaggerRemoteComponent
 
 class App : Application() {
@@ -14,13 +18,25 @@ class App : Application() {
         super.onCreate()
         //Инициализируем экземпляр App, через который будем получать доступ к остальным переменным
         instance = this
-//Создаем компонент
+        //Создаем компонент
         val remoteProvider = DaggerRemoteComponent.create()
         dagger = DaggerAppComponent.builder()
             .remoteProvider(remoteProvider)
             .databaseModule(DatabaseModule())
             .domainModule(DomainModule(this))
             .build()
+        //Задаем имя, описание и важность канала
+        val name = "WatchLaterChannel"
+        val descriptionText = "FilmsSearch notification Channel"
+        val importance = NotificationManager.IMPORTANCE_DEFAULT
+        //Создаем канал, передав в параметры его ID(строка), имя(строка), важность(константа)
+        val mChannel = NotificationChannel(CHANNEL_ID, name, importance)
+        //Отдельно задаем описание
+        mChannel.description = descriptionText
+        //Получаем доступ к менеджеру нотификаций
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        //Регистрируем канал
+        notificationManager.createNotificationChannel(mChannel)
     }
 
     companion object {
---
+++ app/src/main/java/ru/musindev/myapp/view/notifications/NotificationConstans.kt
@@ -0,0 +1,5 @@
+package ru.musindev.myapp.view.notifications
+
+object NotificationConstants {
+    const val CHANNEL_ID = "FilmsSearchChannel"
+}
